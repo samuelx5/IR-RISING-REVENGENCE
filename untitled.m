e45e8a9aa3a1042b46f56f182b34806384337812
samuelx5 <65216389+samuelx5@users.noreply.github.com>
2023-09-29 17:55:05 +1000

    Create untitled.m

---
+++ untitled.m
@@ -0,0 +1,22 @@
+clf;
+clc;
+trOrigin = eye(4)
+%x1 = transl(0,0,10)
+x1 = transl(0,0,-8)
+
+
+
+
+tranimate(trOrigin,x1)
+%x2 = x1*trotx(-30*pi/180)
+x2 = transl(6,6,13)*x1
+tranimate(x1,x2)
+%x3 = transl(0,2,0)*x2
+x3 = transl(-5,3,-2)*x2
+tranimate(x2,x3)
+%grounds = inv(x3)*x3*trotx(-30*pi/180)
+%tranimate(x3,grounds)
+%x4 = grounds*troty(30*pi/180)
+%tranimate(grounds,x4)
+
+
