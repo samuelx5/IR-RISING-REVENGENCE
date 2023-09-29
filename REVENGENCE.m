4c97778bdd0eda53d7da144ad246e9973ca48a69
samuelx5 <65216389+samuelx5@users.noreply.github.com>
2023-09-29 19:22:06 +1000

    Create REVENGENCE.m

---
+++ REVENGENCE.m
@@ -0,0 +1,19 @@
+%----SET VARIABLES------
+% Inputs:
+
+% Outputs:
+%------------------
+
+
+%----DUMMY SENSOR DATA------
+%Inputs:
+
+%Outputs:
+%------------------
+
+%----Solve Pose from Sensor to robot------
+%Inputs:
+
+%Outputs:
+%------------------
+
