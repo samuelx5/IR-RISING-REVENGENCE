fba1f426d61fdd3605dacc670778732d6388b019
samuelx5 <65216389+samuelx5@users.noreply.github.com>
2023-09-30 17:54:57 +1000

    While loop temp

--- REVENGENCE.m
+++ REVENGENCE.m
@@ -1,19 +1,32 @@
 %----SET VARIABLES------
-% Inputs:
+clear
+clc
+E_Stop = 0;
+robot = DobotMagician;
+workspace = [-0.5 0.5 -0.5 0.5 0 0.5]
+q1 = robot.model.ikcon(transl(0,0,0));
+q2 = robot.model.ikcon(transl(0,0,0));
+steps = 10;
 
-% Outputs:
+
 %------------------
 
 
 %----DUMMY SENSOR DATA------
 %Inputs:
-
 %Outputs:
 %------------------
 
 %----Solve Pose from Sensor to robot------
 %Inputs:
-
+while true 
+    if E_Stop == 0
+        q2 = robot.model.ikcon(transl(randn(1),randn(1),randn(1)));
+        qMatrix = jtraj(q1,q2,steps);
+        robot.model.plot(qMatrix,'workspace', workspace, 'trail','r-')
+        q1 = q2;
+    end
+end
 %Outputs:
 %------------------
 
