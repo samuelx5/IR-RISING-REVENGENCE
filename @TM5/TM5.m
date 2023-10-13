ae6b9b5487844a69ce40594451a2f59a970c3fa2
samuelx5 <65216389+samuelx5@users.noreply.github.com>
2023-10-13 15:47:43 +1100

    TM5 skeleton, replace ply files

---
+++ @TM5/TM5.m
@@ -0,0 +1,44 @@
+classdef TM5 < RobotBaseClass
+    %% TM5-700 Omron cobot
+
+
+    properties(Access = public)   
+        plyFileNameStem = 'TM5';
+    end
+    
+    methods
+%% Constructor
+        function self = TM5(baseTr,useTool,toolFilename)
+            if nargin < 3
+                if nargin == 2
+                    error('If you set useTool you must pass in the toolFilename as well');
+                elseif nargin == 0 
+                    baseTr = transl(0,0,0);  
+                end             
+            else % All passed in 
+                self.useTool = useTool;
+                toolTrData = load([toolFilename,'.mat']);
+                self.toolTr = toolTrData.tool;
+                self.toolFilename = [toolFilename,'.ply'];
+            end
+            self.CreateModel();
+			self.model.base = self.model.base.T * baseTr;
+            self.model.tool = self.toolTr;
+            self.PlotAndColourRobot();
+
+            drawnow
+        end
+
+%% CreateModel
+        function CreateModel(self)
+            link(1) = Link('d',0.1452,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]), 'offset',0);
+            link(2) = Link('d',0,'a',-0.329,'alpha',0,'qlim', deg2rad([-360 360]), 'offset',0);
+            link(3) = Link('d',0,'a',-0.3115,'alpha',0,'qlim', deg2rad([-360 360]), 'offset', 0);
+            link(4) = Link('d',0.106,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
+            link(5) = Link('d',0.106,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
+            link(6) = Link('d',0.1132,'a',0,'alpha',0,'qlim',deg2rad([-360,360]), 'offset', 0);
+             
+            self.model = SerialLink(link,'name',self.name);
+        end      
+    end
+end
