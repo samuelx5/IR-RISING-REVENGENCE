e91742131b914eb31295e2bd8643923a2318f16c
Vidushan04 <vidu1040@gmail.com>
2023-10-24 20:14:49 +1100

    Update sensors_assignment.m

--- sensors_assignment.m
+++ sensors_assignment.m
@@ -86,18 +86,19 @@ while(1)
     
     %c.Value = end_effector_position(2);
     
-    
-     end_effector_rotation = [0,0,0];
-     dobot.PublishEndEffectorPose(end_effector_position,end_effector_rotation);
-     pause(1);
+     % 
+     % end_effector_rotation = [0,0,0];
+     % dobot.PublishEndEffectorPose(end_effector_position,end_effector_rotation);
+     % pause(1);
     %dobot.PublishEndEffectorPose(end_effector_position2,end_effector_rotation);
 end
 
 %% 
+% 
+end_effector_position = [0.2287, 0.2266, 0.0448];
 
-end_effector_position = [0.19, -0.1, 0.15];
-% end_effector_position = uicontrol
-% end_effector_position2 = [0.20 ,-0.14,0.1];
+end_effector_position3 = [-0.054 ,-0.3051, -0.0158];
+end_effector_position2 = [0.2, 0, 0];
 
 
 
@@ -106,6 +107,44 @@ end_effector_position = [0.19, -0.1, 0.15];
 
 end_effector_rotation = [0,0,0];
 dobot.PublishEndEffectorPose(end_effector_position,end_effector_rotation);
-% pause(10);
-% dobot.PublishEndEffectorPose(end_effector_position2,end_effector_rotation);
+pause(5);
+dobot.PublishEndEffectorPose(end_effector_position2,end_effector_rotation);
+pause(5);
+dobot.PublishEndEffectorPose(end_effector_position3,end_effector_rotation);
+%% 
+
+
+% Get the initial joint positions
+jointStateSubscriber = rossubscriber('/dobot_magician/joint_states'); % Create a ROS Subscriber to the topic joint_states
+pause(2); % Allow some time for a message to appear
+initialJointPositions = jointStateSubscriber.LatestMessage.Position % Get the latest message
+
+% Record the starting time
+startTime = now;  % Use 'now' to get the current time
+
+% Move the robot to a new joint configuration
+newJointPositions = [0.1, 0.2, 0.3, 0.4];  % Replace with the desired joint positions
+dobot.PublishTargetJoint(newJointPositions);
+
+% Wait for a short time if needed
+% pause(2);  % Adjust the time delay as needed
+
+% Get the final joint positions
+finalJointPositions = jointStateSubscriber.LatestMessage.Position;
+
+% Calculate the displacement
+displacement = finalJointPositions - initialJointPositions;
+
+% Calculate the time elapsed
+endTime = now;
+timeElapsed = (endTime - startTime) * 24 * 3600;  % Convert from days to seconds
+
+% Calculate the speed (in joint units per second)
+speed = displacement / timeElapsed;
+
+disp("Joint Speeds: ");
+disp(speed);
+
+% Clean up, stop the robot, or perform other actions as needed
+
 
