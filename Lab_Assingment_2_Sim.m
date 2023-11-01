0d3f009e21a34be81c7f1fdb69ce130b66a60762
Rezaki8364 <146419994+Rezaki8364@users.noreply.github.com>
2023-11-01 18:10:45 +1100

    Movement of both robots in same space consecutively

--- Lab_Assingment_2_Sim.m
+++ Lab_Assingment_2_Sim.m
@@ -9,23 +9,21 @@ bot.model.base = transl([0.5,0,0]);
 robot.model.plot(q,'workspace',workspace,'scale',scale);
 bot.model.plot(r,'workspace',workspace,'scale',scale); 
 
-% T1 = transl(0.5,0.4,0.5);
-% q1 = robot.model.ikcon(T1);
-% T2 = transl(0.3,0.4,0.3);
-% q2 = robot.model.ikcon(T2);
-% T3 = transl(-0.4,-0.6,0.3);
-% q3 = bot.model.ikcon(T3);
-% T4 = transl(-0.7,0.1,0.3);
-% q4 = bot.model.ikcon(T4);
-% % 
-% steps = 50;
-% 
-% qMatrix = jtraj(q1,q2,steps);
-% qMatrix3 = jtraj(q3,q4,steps);
-% 
-% figure(1)
-% robot.model.plot(qMatrix,'workspace',workspace,'trail','r-') 
-% pause(3);
-% robot.model.plot(qMatrix2, 'workspace',workspace,'trail','r-')
-% pause(3);
-% robot.model.plot(qMatrix3, 'workspace',workspace,'trail','r-')
+T1 = transl(-0.1,0,0.5);
+q1 = robot.model.ikcon(T1);
+T2 = transl(-0.9,0.4,0.3);
+q2 = robot.model.ikcon(T2);
+T3 = transl(0.4,0,0.3);
+q3 = bot.model.ikcon(T3);
+T4 = transl(0.7,0.1,0.3);
+q4 = bot.model.ikcon(T4);
+
+steps = 50;
+
+qMatrix = jtraj(q1,q2,steps);
+qMatrix1 = jtraj(q3,q4,steps);
+
+figure(1)
+robot.model.plot(qMatrix,'workspace',workspace,'trail','r-') 
+bot.model.plot(qMatrix1, 'workspace',workspace,'trail','r-')
+
